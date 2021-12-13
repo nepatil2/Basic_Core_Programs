cf3596741cfdfb3b906fae61569d6b6b76af77f2
nepatil2 <nehapatil22899@gmail.com>
2021-12-13 21:47:17 +0530

    Even Odd

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+package com.bl.examples;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int num = sc.nextInt();
+
+        if(num%2 == 0){
+            System.out.println("Number is Even :" + num);
+        }
+        else
+            System.out.println("Number is Odd : " + num);
+    }
+}
